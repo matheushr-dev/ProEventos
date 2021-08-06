814ead5ec11dddbf80c2e6a939611e7497ea29e3
Matheus Henrique Rodrigues <matheus.rodrigues@sequoia.local>
2021-08-06 13:19:45 -0300

    Seção 2: Dotnet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+         public IEnumerable<Evento> _evento = new Evento[] {new Evento(){
+                    EventoId = 1,
+                    Tema = "angular 11 e .net 5",
+                    Local = "Itapevi",
+                    Lote = "1º lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+               },
+               new Evento(){
+                    EventoId = 2,
+                    Tema = "angular e suas novidades",
+                    Local = "São Paulo",
+                    Lote = "2º lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyy"),
+                    ImagemURL = "foto2.png"
+               }
+         };
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id) 
+        {
+           return _evento.Where(x => x.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+           return "Exemplo de Post";
+        }
+    }
+}
