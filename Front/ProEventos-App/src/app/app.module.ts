7d598ec1727843d510e246e6d1893c37836283dd
Matheus Henrique Rodrigues <matheushenri4141@gmail.com>
2021-08-20 14:44:46 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http' //Modulo de add CHamadas HTTP dentro do componente
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [ //Componentes
+  declarations: [	 //Componentes
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+      NavComponent
    ],
   imports: [ //Modulos
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, //Modulo de add CHamadas HTTP dentro do componente
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
